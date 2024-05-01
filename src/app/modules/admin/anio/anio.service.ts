a0c8da845c6f2d964194f20d490af68c9a06b82c
ridjeim <dgomay27@gmail.com>
2024-05-01 15:52:04 -0500

    filtro cupo / sustancia2

--- src/app/modules/admin/anio/anio.service.ts
+++ src/app/modules/admin/anio/anio.service.ts
@@ -8,7 +8,7 @@ import { Anio } from './anio.model';
   providedIn: 'root'
 })
 export class AnioService {
-  private apiUrl = environment.apiUrl+'/anios';
+  private apiUrl = `${environment.apiUrl}/anios`;
 
   constructor(private http: HttpClient) { }
 
